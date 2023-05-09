eec834a8428fd77befe6de537aaeabe9ecf86c29
Toan Trinh <toan.trinh@ptnglobalcorp.com>
2023-05-09 10:25:29 +0700

    update all data

--- scripts/etcd.sh
+++ scripts/etcd.sh
@@ -1,27 +1,74 @@
-#!/usr/bin/env bash
+# # !/usr/bin/env bash
+
+# echo "installing etcd operator"
+# # kubectl  create -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/deployment.yaml
+# # kubectl  rollout status -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/deployment.yaml
+# kubectl  create -f scripts/deployment.yml
+# kubectl  rollout status -f scripts/deployment.yml
+# until kubectl  get thirdpartyresource cluster.etcd.coreos.com
+# do
+#     echo "waiting for operator"
+#     sleep 2
+# done
+
+# echo "pausing for 10 seconds for operator to settle"
+# sleep 10
+
+# kubectl  create -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/example-etcd-cluster.yaml
+
+# echo "installing etcd cluster service"
+# kubectl  create -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/example-etcd-cluster-nodeport-service.json
+
+# echo "waiting for etcd cluster to turnup"
+
+# until kubectl  get pod example-etcd-cluster-0002
+# do
+#     echo "waiting for etcd cluster to turnup"
+#     sleep 2
+# done
+
 
 echo "installing etcd operator"
-kubectl  create -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/deployment.yaml
-kubectl  rollout status -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/deployment.yaml
-
-until kubectl  get thirdpartyresource cluster.etcd.coreos.com
-do
-    echo "waiting for operator"
-    sleep 2
-done
-
+kubectl create -f scripts/deployment.yml
+kubectl rollout status deployment/etcd-operator
 echo "pausing for 10 seconds for operator to settle"
 sleep 10
-
-kubectl  create -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/example-etcd-cluster.yaml
-
+kubectl create -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/example-etcd-cluster.yaml
 echo "installing etcd cluster service"
-kubectl  create -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/example-etcd-cluster-nodeport-service.json
-
+kubectl create -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/example-etcd-cluster-nodeport-service.json
 echo "waiting for etcd cluster to turnup"
-
-until kubectl  get pod example-etcd-cluster-0002
+until kubectl get pod example-etcd-cluster-0002
 do
     echo "waiting for etcd cluster to turnup"
     sleep 2
 done
+
+
+
+
+# echo "installing etcd operator"
+# kubectl apply -f https://raw.githubusercontent.com/coreos/etcd-operator/master/deploy/etcd-operator.yaml
+# kubectl rollout status deployment/etcd-operator
+
+# until kubectl get crd | grep cluster.etcd.coreos.com
+# do
+#     echo "waiting for operator"
+#     sleep 2
+# done
+
+# echo "pausing for 10 seconds for operator to settle"
+# sleep 10
+
+# kubectl apply -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/example-etcd-cluster.yaml
+
+# echo "installing etcd cluster service"
+# kubectl apply -f https://raw.githubusercontent.com/coreos/etcd-operator/master/example/example-etcd-cluster-nodeport-service.yaml
+
+# echo "waiting for etcd cluster to turn up"
+
+# until kubectl get pod example-etcd-cluster-0002
+# do
+#     echo "waiting for etcd cluster to turn up"
+#     sleep 2
+# done
+
