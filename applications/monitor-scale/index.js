1023fef547ba601c22ec4c70e5f348627376e232
Toan Trinh <toan.trinh@ptnglobalcorp.com>
2023-05-16 11:02:07 +0700

    update

--- applications/monitor-scale/index.js
+++ applications/monitor-scale/index.js
@@ -32,7 +32,7 @@ app.post('/scale', function (req, res) {
   var scale = req.body.count;
   
   console.log('Count requested is: %s', scale);
-  Console.log('33aaaaaaaaaaaaaaaaaaa')
+  console.log('33aaaaaaaaaaaaaaaaaaa')
   var url = "http://host.docker.internal:2345/apis/extensions/v1beta1/namespaces/default/deployments/puzzle/scale";
   // var putBody = {
   //   // kind:"Scale",
